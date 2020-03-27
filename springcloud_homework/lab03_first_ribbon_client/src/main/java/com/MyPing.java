85e028446ea392c34c8d7ae18d3e298a67ca8cf5
SouthGU <2370015271@qq.com>
2020-03-27 23:30:03 +0800

    MyPing --> 2020/3/27.

--- springcloud_homework/lab03_first_ribbon_client/src/main/java/com/MyPing.java
+++ springcloud_homework/lab03_first_ribbon_client/src/main/java/com/MyPing.java
@@ -1,9 +1,18 @@
 package com;
 
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.Server;
+
 /**
  * @Auther: sise.xgl
  * @Date: 2020/3/26/10:50
  * @Description:
  */
-public class MyPing {
+public class MyPing implements IPing {
+
+    @Override
+    public boolean isAlive(Server server) {
+        System.out.println("This is my Ping class: " + server.getHostPort());
+        return true;
+    }
 }
--- springcloud_homework/lab03_first_ribbon_client/src/main/java/com/TestPingUrl.java
+++ springcloud_homework/lab03_first_ribbon_client/src/main/java/com/TestPingUrl.java
@@ -1,8 +1,11 @@
 package com;
 
+import com.netflix.client.ClientFactory;
+import com.netflix.config.ConfigurationManager;
 import com.netflix.loadbalancer.BaseLoadBalancer;
 import com.netflix.loadbalancer.PingUrl;
 import com.netflix.loadbalancer.Server;
+import com.netflix.niws.client.http.RestClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,22 +17,32 @@ import java.util.List;
  */
 public class TestPingUrl {
     public static void main(String[] args) throws  Exception {
-        BaseLoadBalancer Ib = new BaseLoadBalancer();
-        List<Server> servers = new ArrayList<Server>();
-        servers.add(new Server("localhost",8081));
-        servers.add(new Server("localhost",8888));
 
-        Ib.addServers(servers);
+//        BaseLoadBalancer Ib = new BaseLoadBalancer();
+//        List<Server> servers = new ArrayList<Server>();
+//        servers.add(new Server("localhost",8081));
+//        servers.add(new Server("localhost",8888));
+//
+//        Ib.addServers(servers);
+//
+//
+//        Ib.setPing(new PingUrl());
+//        Ib.setPingInterval(2);
+//
+//        Thread.sleep(6000);
+//        for (Server s:Ib.getAllServers()){
+//            System.out.println(s.getHostPort()+" State"+s.isAlive());
+//        }
 
 
-        Ib.setPing(new PingUrl());
-        Ib.setPingInterval(2);
-
+        ConfigurationManager.loadPropertiesFromResources("application.yml");
+        RestClient client = (RestClient) ClientFactory.getNamedClient("my-client");
         Thread.sleep(6000);
-        for (Server s:Ib.getAllServers()){
-            System.out.println(s.getHostPort()+" State"+s.isAlive());
+        List<Server> servers = client.getLoadBalancer().getAllServers();
+        System.out.println(servers.size());
+        for (Server s: servers){
+            System.out.println(s.getHostPort()+"State: "+ s.isAlive());
         }
-
 
     }
 }
