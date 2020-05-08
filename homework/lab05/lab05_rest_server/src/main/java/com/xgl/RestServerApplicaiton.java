943b6c632a0fa240c8c0d3af489b5ebec94fbb4a
SouthGU <2370015271@qq.com>
2020-05-08 16:48:18 +0800

    rest-server --> 2020/5/8.

---
+++ homework/lab05/lab05_rest_server/src/main/java/com/xgl/RestServerApplicaiton.java
@@ -0,0 +1,16 @@
+package com.xgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: sise.xgl
+ * @Date: 2020/5/21/10:52
+ * @Description: 服务端
+ */
+@SpringBootApplication
+public class RestServerApplicaiton {
+    public static void main(String[] args) {
+        SpringApplication.run(RestServerApplicaiton.class,args);
+    }
+}
