615f40f5d30ea3332a18f892fc19c7361f8c8486
SouthGU <2370015271@qq.com>
2020-05-10 16:53:19 +0800

    spring-feign-invoker-2 --> 2020/5/10.

---
+++ homework/lab06/lab06_spring_feign_invoker/src/main/java/com/xgl/HelloClient.java
@@ -0,0 +1,22 @@
+//package com.xgl;
+//
+//import org.springframework.cloud.openfeign.FeignClient;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+///**
+// * @Auther: sise.xgl
+// * @Date: 2020/5/30/22:36
+// * @Description:
+// */
+//
+//@FeignClient(name = "spring-feign-provider")
+//public interface HelloClient {
+//
+//    @MyUrl(method = "GET",url = "/hello")
+//    String myHello();
+//
+//    @RequestMapping(method = RequestMethod.GET,value = "/hello")
+//    String springHello();
+//}
