fa9298ca4645ae8b81971e8432250ddb47aebfff
SouthGU <2370015271@qq.com>
2020-05-13 17:30:38 +0800

    ache-update --> 2020/5/13.

--- springcloud_homework/lab05/lab05_feign_client/src/main/java/com/xgl/FeignClientApplication.java
+++
@@ -1,9 +0,0 @@
-package com.xgl;
-
-/**
- * @Auther: sise.xgl
- * @Date: 2020/5/25/9:17
- * @Description:
- */
-public class FeignClientApplication {
-}
--- springcloud_homework/lab09/lab09_spring_hystrix_invoker/src/main/java/com/xgl/MyException.java
+++
@@ -1,9 +0,0 @@
-package com.xgl;
-
-/**
- * @Auther: sise.xgl
- * @Date: 2020/5/31/11:51
- * @Description:
- */
-public class MyException extends Throwable {
-}
