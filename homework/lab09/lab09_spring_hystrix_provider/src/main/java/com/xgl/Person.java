6d6b963fff1e9d4b832fc3e35ee319400fe6856b
SouthGU <2370015271@qq.com>
2020-05-16 18:06:39 +0800

    spring-hystrix-provider --> 2020/5/16.

---
+++ homework/lab09/lab09_spring_hystrix_provider/src/main/java/com/xgl/Person.java
@@ -0,0 +1,27 @@
+package com.xgl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Auther: sise.xgl
+ * @Date: 2020/5/31/11:01
+ * @Description:
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String message;
+
+    public Person(Integer id, String name, Integer age) {
+        super();
+        this.id = id ;
+        this.name = name;
+        this.age = age;
+    }
+}
